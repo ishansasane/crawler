05acfa00d6d7a0a108d411f0679c5dc12f3489fa
Ishna Sasane <ishansasane999@gmail.com>
2025-06-06 19:36:57 +0530

    added mongodb for crawl db

--- utils/url_utils.go
+++ utils/url_utils.go
@@ -4,11 +4,11 @@ import (
 	"net/url"
 )
 
-func IsSameDomain(base string, other string) bool {
-    u1, err1 := url.Parse(base)
-    u2, err2 := url.Parse(other)
+func IsSameDomain(base, target string) bool {
+    baseURL, err1 := url.Parse(base)
+    targetURL, err2 := url.Parse(target)
     if err1 != nil || err2 != nil {
         return false
     }
-    return u1.Hostname() == u2.Hostname()
+    return baseURL.Hostname() == targetURL.Hostname()
 }
